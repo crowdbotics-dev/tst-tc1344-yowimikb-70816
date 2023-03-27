76b837edf59fbb9ac135ca35ebd1c11f7c752144
Arun Kumar <arun.kumar@crowdbotics.com>
2023-03-28 03:06:31 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "tst-tc1344-yowimikb-70816",
-  url: "https://tst-tc1344-yowimikb-70816.botics.co",
-  api: "https://tst-tc1344-yowimikb-70816.botics.co/api/v1"
-}
+  "name": "tst-tc1344-yowimikb-70816",
+  "url": "https://tst-tc1344-yowimikb-70816.botics.co",
+  "api": "https://tst-tc1344-yowimikb-70816.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
